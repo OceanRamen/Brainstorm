90936c99d2693706b9ce59ab391766f7e680226d
OceanRamen <wt557978@gmail.com>
2024-05-21 01:34:58 +0100

    added autoreroll for tag functionality

--- README.md
+++ README.md
@@ -13,9 +13,19 @@ Note: Brainstorm requires [lovely](https://github.com/ethangreen-dev/lovely-inje
 - Hold `z` + 0-6 to save a state to slot
 - Hold `x`+ 0-6 to load a state from slot
 - Press `t` to reroll the seed
+- Press `a` to auto-reroll for a certain tag (Default: charm tag)
 
 # How to adjust keybinds:
 1. Open up `main.lua`
 2. Locate the line `-- KEYBINDS --`
 3. Adjust the keybind variables to change to your desired keybinds -- Make sure to leave in the quotation marks
 4. Save and exit the file, your keybinds will now be aplied when you next reload the mod! 
+
+# How to change tag to autoreroll for:
+1. Open up `main.lua`
+2. Locate the line `-- AUTOREROLL CONFIG --`
+3. Change the `searchTag` var to your chosen tag, using the correct name as specified in the key below.
+4. Save and exit the file, and reload the game.
+
+Example:
+`searchTag = "tag_charm"` -> `searchTag = "tag_rare"`
--- main.lua
+++ main.lua
@@ -1,5 +1,32 @@
 local global = {}
 
+-- AUTOREROLL CONFIG --
+searchTag = "tag_charm"
+
+-- tag_uncommon     =     'Uncommon Tag'
+-- tag_rare         =     'Rare Tag'
+-- tag_negative     =     'Negative Tag'
+-- tag_holo         =     'Holographic Tag'
+-- tag_polychrome   =     'Polychrome Tag'
+-- tag_investment   =     'Investment Tag'
+-- tag_voucher      =     'Voucher Tag'
+-- tag_boss         =     'Boss Tag'
+-- tag_standard     =     'Standard Tag'
+-- tag_charm        =     'Charm Tag'
+-- tag_meteor       =     'Meteor Tag'
+-- tag_buffoon      =     'Buffoon Tag'  
+-- tag_handy        =     'Handy Tag'
+-- tag_garbage      =     'Garbage Tag'
+-- tag_ethereal     =     'Ethereal Tag'
+-- tag_coupon       =     'Coupon Tag'
+-- tag_double       =     'Double Tag'
+-- tag_juggle       =     'Juggle Tag'
+-- tag_d_six        =     'D6 Tag'    
+-- tag_top_up       =     'Top-up Tag' 
+-- tag_skip         =     'Skip Tag'
+-- tag_orbital      =     'Orbital Tag'
+-- tag_economy      =     'Economy Tag'
+
 -- KEYBINDS --
 keybinds = {    
     saveState="z",
@@ -14,10 +41,35 @@ function _reroll()
     G:start_run({stake = _stake})
 end
 
-local _soul = false
-local _charm = false
+function searchParametersMet()
+    if not G or not G.GAME or not G.GAME.round_resets or not G.GAME.round_resets.blind_tags then
+        print("One or more variables are nil or undefined")
+        return false
+    end
 
-local sKeys = {"1", "2", "3", "4", "5", "6"}   
+    local _tag = G.GAME.round_resets.blind_tags.Small
+    if not _tag then
+        print("Value of _tag is nil or undefined")
+        return false
+    end
+
+    if _tag == searchTag then
+        -- Check if arcana pack from skip has soul in
+        return true
+    else
+        return false
+    end
+end
+
+
+local sKeys = {"1", "2", "3", "4", "5", "6"}  
+
+-- Add a flag to track if auto reroll is active
+local autoRerollActive = false
+local rerollInterval = 0.01  -- Time interval between rerolls (in seconds)
+local rerollTimer = 0
+
+
 function global.keyHandler(controller, key, dt)
     for i, k in ipairs(sKeys) do
         if key == k and love.keyboard.isDown(keybinds.saveState) then
@@ -35,6 +87,23 @@ function global.keyHandler(controller, key, dt)
     if key == keybinds.rerollSeed then
         _reroll()
     end
+    if key == keybinds.autoreroll then
+        autoRerollActive = not autoRerollActive
+    end
 end
+
+function global.update(dt)
+    if autoRerollActive then
+        rerollTimer = rerollTimer + dt
+        if rerollTimer >= rerollInterval then
+            rerollTimer = rerollTimer - rerollInterval
+            _reroll()
+            if searchParametersMet() then
+                autoRerollActive = false
+            end
+        end
+    end
+end
+
 
 return global
